2c68a876c4fde821304eed06401fe9823cda6962
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2023-10-29 15:34:26 +0100

    ConfigurationProvider: listen to configuration changes

--- src/main/kotlin/dev/aibtra/configuration/ConfigurationProvider.kt
+++ src/main/kotlin/dev/aibtra/configuration/ConfigurationProvider.kt
@@ -4,9 +4,13 @@
 
 package dev.aibtra.configuration
 
+import kotlin.reflect.KClass
+
 interface ConfigurationProvider {
 
 	fun <D> get(factory: ConfigurationFactory<D>): D
 
 	fun <T> change(factory: ConfigurationFactory<T>, change: (T) -> T)
+
+	fun <T : Any> listenTo(clazz: KClass<T>, runnable: Runnable)
 }
