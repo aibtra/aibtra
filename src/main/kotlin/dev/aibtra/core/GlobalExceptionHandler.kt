5e7a0b47b0a09c7c62550243fe14bd866b3771a6
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2023-11-07 10:03:02 +0100

    GlobalExceptionHandler: write "bug" file

--- src/main/kotlin/dev/aibtra/core/GlobalExceptionHandler.kt
+++ src/main/kotlin/dev/aibtra/core/GlobalExceptionHandler.kt
@@ -7,11 +7,15 @@ package dev.aibtra.core
 import dev.aibtra.gui.Ui
 import dev.aibtra.gui.dialogs.DialogDisplayer
 import dev.aibtra.gui.dialogs.Dialogs
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class GlobalExceptionHandler {
 
 	companion object {
 		private val LOG = Logger.getLogger(this::class)
+		private val DATE_FORMAT: DateFormat = SimpleDateFormat("yyyyMMddHHmmss")
 
 		fun install() {
 			Thread.setDefaultUncaughtExceptionHandler { _, e -> handle(e) }
@@ -19,6 +23,8 @@ class GlobalExceptionHandler {
 
 		fun handle(th: Throwable) {
 			LOG.error(th)
+
+			Logger.backup("bug-" + DATE_FORMAT.format(Date()))
 
 			Ui.runInEdt {
 				Dialogs.showError("Internal Error", "An internal error has occurred. Please report at:\n\nhttps://github.com/aibtra/aibtra/issues", DialogDisplayer.createGlobal())
--- src/main/kotlin/dev/aibtra/core/Logger.kt
+++ src/main/kotlin/dev/aibtra/core/Logger.kt
@@ -4,6 +4,11 @@
 
 package dev.aibtra.core
 
+import dev.aibtra.main.startup.MainStartup
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.util.logging.FileHandler
 import java.util.logging.Level
 import java.util.logging.Logger
 import kotlin.reflect.KClass
@@ -30,8 +35,29 @@ class Logger private constructor(private val logger: Logger) {
 	}
 
 	companion object {
+		private var logFile: Path? = null
+		private var fileHandler: FileHandler? = null
+
 		fun getLogger(kClass: KClass<*>): dev.aibtra.core.Logger {
 			return Logger(Logger.getLogger(kClass.qualifiedName))
+		}
+
+		fun setup(logFile: Path) {
+			this.logFile = logFile
+
+			val logger: Logger = Logger.getLogger("")
+			fileHandler = FileHandler(logFile.toString()).apply {
+				formatter = MainStartup.LogFormatter()
+			}
+			logger.addHandler(fileHandler)
+			logger.level = Level.INFO
+		}
+
+		fun backup(prefix: String) {
+			fileHandler?.flush()
+			logFile?.let {
+				Files.copy(it, Files.createTempFile(it.parent, "$prefix-", ".txt"), StandardCopyOption.REPLACE_EXISTING)
+			}
 		}
 	}
 }
