992a542802ad0d48f1cc8b8e48d56739504b8291
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-05-25 10:06:01 +0200

    OpenAIConfiguration: switch default model to GPT-4o

--- src/main/kotlin/dev/aibtra/openai/OpenAIConfiguration.kt
+++ src/main/kotlin/dev/aibtra/openai/OpenAIConfiguration.kt
@@ -10,25 +10,18 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class OpenAIConfiguration(
 	val apiToken: String? = null,
-	val profiles: List<Profile> = listOf(CORRECTION_GPT_3_5_TURBO, CORRECTION_GPT_4, TO_STANDARD_ENGLISH),
-	val defaultProfileName: String = PROOFREAD_3_5
+	val profiles: List<Profile> = listOf(PROOFREAD, TO_STANDARD_ENGLISH),
+	val defaultProfileName: String = PROOFREAD_TITLE
 ) {
 	@Serializable
 	class Profile(val name: String, val model: String, val instructions: String)
 
 	companion object : ConfigurationFactory<OpenAIConfiguration> {
-		const val PROOFREAD_3_5 = "Proofread (GPT-3.5 Turbo)"
+		const val PROOFREAD_TITLE = "Proofread (GPT-4o)"
+		private const val MODEL_4O = "gpt-4o"
 
-		private val CORRECTION_GPT_3_5_TURBO = Profile(
-			PROOFREAD_3_5, "gpt-3.5-turbo",
-			"Correct typos and grammar in the markdown following " +
-					"AND stay as close as possible to the original " +
-					"AND preserve block quotes and code blocks " +
-					"AND preserve the detected language:"
-		)
-
-		private val CORRECTION_GPT_4 = Profile(
-			"Proofread (GPT-4)", "gpt-4",
+		private val PROOFREAD = Profile(
+			PROOFREAD_TITLE, MODEL_4O,
 			"Correct typos and grammar in the markdown following " +
 					"AND stay as close as possible to the original " +
 					"AND do not change the markdown structure " +
@@ -36,7 +29,7 @@ data class OpenAIConfiguration(
 		)
 
 		private val TO_STANDARD_ENGLISH = Profile(
-			"To Standard English (GPT-3.5 Turbo)", "gpt-3.5-turbo",
+			"To Standard English (GPT-4o)", MODEL_4O,
 			"Rewrite to Standard English " +
 					"BUT stay as close as possible to the original:"
 		)
