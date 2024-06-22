e3f46fb66343601035a481b7cbe39078c31b41bf
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-06-22 21:32:05 +0200

    UpdateCheck: should honor bundle kind

--- src/main/kotlin/dev/aibtra/main/frame/BuildInfo.kt
+++ src/main/kotlin/dev/aibtra/main/frame/BuildInfo.kt
@@ -6,13 +6,38 @@
 
 package dev.aibtra.main.frame
 
+import dev.aibtra.core.Logger
+import dev.aibtra.gui.Ui
 import dev.aibtra.main.startup.MainStartup
+import java.lang.IllegalArgumentException
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.*
 
-class BuildInfo(val sha: String, val instant: String) {
+class BuildInfo(val sha: String, val instant: String, val bundleType: BundleType?) {
 	companion object {
+		private val LOG = Logger.getLogger(this::class)
+
 		fun load() : BuildInfo {
-			MainStartup::class.java.getResourceAsStream("/build.properties").use {
+			val javaClass = MainStartup::class.java
+			val jarPath = Path.of(javaClass.protectionDomain.codeSource.location.toURI())
+			val bundleFile = jarPath.parent.resolve("aibtra.bundletype")
+
+			val bundleType = if (Files.isRegularFile(bundleFile)) {
+				val fileContent = Files.readString(bundleFile).trim()
+				try {
+					BundleType.valueOf(fileContent)
+				} catch (e: IllegalArgumentException) {
+					LOG.error("Invalid bundle type $fileContent")
+					BundleType.stable
+				}
+			} else {
+				if (Ui.isDebugging()) BundleType.experimental else BundleType.stable
+			}
+
+			LOG.info("Bundle type is $bundleType")
+
+			javaClass.getResourceAsStream("/build.properties").use {
 				val properties = Properties()
 				if (it != null) {
 					properties.load(it)
@@ -20,8 +45,12 @@ class BuildInfo(val sha: String, val instant: String) {
 
 				val sha = properties.getProperty("sha") ?: "<unknown>"
 				val instant = properties.getProperty("time") ?: "unknown"
-				return BuildInfo(sha, instant)
+				return BuildInfo(sha, instant, bundleType)
 			}
 		}
+	}
+
+	enum class BundleType {
+		stable, latest, experimental
 	}
 }
