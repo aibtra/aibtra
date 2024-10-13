dafdd64374132c4954d400bb6146e4fd02b0aacd
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-10-13 17:47:27 +0200

    #5 OpenAI: support for "system role" vs. "user role"

--- src/main/kotlin/dev/aibtra/openai/OpenAIConfiguration.kt
+++ src/main/kotlin/dev/aibtra/openai/OpenAIConfiguration.kt
@@ -18,12 +18,21 @@ data class OpenAIConfiguration(
 	data class Profile(
 		val name: Name,
 		val model: String,
-		val instructions: String,
-		val supportsSchemes: Boolean = false
+		val supportsSchemes: Boolean = false,
+		val instructions: List<Instruction>
 	) {
 
 		@Serializable
 		data class Name(val id: String, val title: String)
+	}
+
+	@Serializable
+	data class Instruction(val role: Role, val text: String)
+
+	@Suppress("unused")
+	@Serializable
+	enum class Role(val id: String) {
+		USER("user"), SYSTEM("system"), ASSISTANT("assistant")
 	}
 
 	fun currentProfile(): Profile {
@@ -32,34 +41,51 @@ data class OpenAIConfiguration(
 
 	companion object : ConfigurationFactory<OpenAIConfiguration> {
 		private const val PROOFREAD_ID = "proofread"
+		const val CONTENT_KEYWORD = "CONTENT"
+		private const val CONTENT_MACRO = "\${${CONTENT_KEYWORD}}"
 		private const val MODEL_4O = "gpt-4o"
 
 		private val PROOFREAD = Profile(
 			Profile.Name(PROOFREAD_ID, "Proofread (GPT-4o)"),
 			MODEL_4O,
-			"Correct typos and grammar in the markdown following " +
-							"AND stay as close as possible to the original " +
-							"AND do not change the markdown structure " +
-							"AND preserve the detected language:",
-			true
+			true,
+			listOf(
+				Instruction(
+					Role.USER, "Correct typos and grammar in the markdown following " +
+									"AND stay as close as possible to the original " +
+									"AND do not change the markdown structure " +
+									"AND preserve the detected language:"
+				),
+				Instruction(Role.USER, CONTENT_MACRO)
+			)
 		)
 
 		private val IMPROVE = Profile(
 			Profile.Name("improve", "Improve Text (GPT-4o)"),
 			MODEL_4O,
-			"Proofread " +
-							"AND improve wording, but stay close to the original, only apply changes to quite uncommon wording " +
-							"AND do not change the markdown structure or indentation or other special symbols " +
-							"AND preserve the detected language:",
-			true
+			true,
+			listOf(
+				Instruction(
+					Role.USER, "Proofread " +
+									"AND improve wording, but stay close to the original, only apply changes to quite uncommon wording " +
+									"AND do not change the markdown structure or indentation or other special symbols " +
+									"AND preserve the detected language:"
+				),
+				Instruction(Role.USER, CONTENT_MACRO)
+			)
 		)
 
 		private val TO_STANDARD_ENGLISH = Profile(
 			Profile.Name("to-standard-english", "To Standard English (GPT-4o)"),
 			MODEL_4O,
-			"Rewrite to Standard English " +
-							"BUT stay as close as possible to the original:",
-			true
+			true,
+			listOf(
+				Instruction(
+					Role.USER, "Rewrite to Standard English " +
+									"BUT stay as close as possible to the original:"
+				),
+				Instruction(Role.USER, CONTENT_MACRO)
+			)
 		)
 
 		override fun name(): String = "openai"
