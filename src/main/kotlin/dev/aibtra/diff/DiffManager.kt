c7c6e1426804c738c54d0c41d7e22e8f27dbc03e
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-11-02 17:07:35 +0100

    Refactoring DiffManager: Config field should not have default values
    It's too easy to confuse boolean parameters.

--- src/main/kotlin/dev/aibtra/diff/DiffManager.kt
+++ src/main/kotlin/dev/aibtra/diff/DiffManager.kt
@@ -388,8 +388,8 @@ class DiffManager(
 
 	@Serializable
 	data class Config(
-		val filterMarkdown: Boolean = true,
-		val showRefBeforeAndAfter: Boolean = true,
+		val filterMarkdown: Boolean,
+		val showRefBeforeAndAfter: Boolean
 	)
 
 	fun interface RawNormalizer {
