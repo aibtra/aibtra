81202dc5c70c3b7ae8d8303007be8d41f6a6cb04
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-11-14 12:12:30 +0100

    #8 Dark mode: improve colors

--- src/main/kotlin/dev/aibtra/main/frame/GuiColors.kt
+++ src/main/kotlin/dev/aibtra/main/frame/GuiColors.kt
@@ -20,8 +20,8 @@ import java.awt.Color
 
 @Serializable
 data class GuiColors(
-	@SerialName("light.4") val light: Colors = Colors.DEFAULTS_LIGHT,
-	@SerialName("dark.4") val dark: Colors = Colors.DEFAULTS_DARK,
+	@SerialName("light.5") val light: Colors = Colors.DEFAULTS_LIGHT,
+	@SerialName("dark.5") val dark: Colors = Colors.DEFAULTS_DARK,
 ) {
 
 	@Serializable
@@ -29,7 +29,8 @@ data class GuiColors(
 		val rawBackgroundModified: Color, val rawBackgroundAdded: Color, val rawBackgroundRemoved: Color, val rawBackgroundRemovedShadow: Color,
 		val refBackgroundModified: Color, val refBackgroundAdded: Color, val refBackgroundRemoved: Color, val refBackgroundRemovedShadow: Color,
 		val selectionColor: Color,
-		val textColor: Color,
+		val foregroundColor: Color,
+		val backgroundColor: Color
 	) {
 		companion object {
 			private val LIGHT_GREEN = Color(0x90, 0xEE, 0x90)
@@ -37,16 +38,18 @@ data class GuiColors(
 			private val LIGHT_RED = Color(0xFF, 0x80, 0x80)
 			private val LIGHT_RED_SHADOW = Color(0xFF, 0x80, 0x80, SHADOW_ALPHA)
 			private val LIGHT_SELECTION = Color(0xCC, 0xCC, 0xCC)
-			private val LIGHT_TEXT = Color(0x10, 0x10, 0x10)
+			private val LIGHT_FOREGROUND = Color(0x10, 0x10, 0x10)
+			private val LIGHT_BACKGROUND = Color(0xFF, 0xFF, 0xFF)
 			private val DARK_GREEN = Color(0x25, 0xA6, 0x25)
 			private val DARK_GREEN_SHADOW = Color(0x25, 0xA6, 0x25, SHADOW_ALPHA)
 			private val DARK_RED = Color(0xC6, 0x28, 0x28)
 			private val DARK_RED_SHADOW = Color(0xC6, 0x28, 0x28, SHADOW_ALPHA)
 			private val DARK_SELECTION = Color(0x70, 0x70, 0x70)
-			private val DARK_TEXT = Color(0xCC, 0xCC, 0xCC)
+			private val DARK_FOREGROUND = Color(0xD0, 0xD0, 0xD0)
+			private val DARK_BACKGROUND = Color(0x10, 0x10, 0x20)
 
-			val DEFAULTS_LIGHT = Colors(LIGHT_RED, LIGHT_RED, LIGHT_GREEN, LIGHT_GREEN_SHADOW, LIGHT_RED, LIGHT_GREEN, LIGHT_RED, LIGHT_RED_SHADOW, LIGHT_SELECTION, LIGHT_TEXT)
-			val DEFAULTS_DARK = Colors(DARK_RED, DARK_RED, DARK_GREEN, DARK_GREEN_SHADOW, DARK_RED, DARK_GREEN, DARK_RED, DARK_RED_SHADOW, DARK_SELECTION, DARK_TEXT)
+			val DEFAULTS_LIGHT = Colors(LIGHT_RED, LIGHT_RED, LIGHT_GREEN, LIGHT_GREEN_SHADOW, LIGHT_RED, LIGHT_GREEN, LIGHT_RED, LIGHT_RED_SHADOW, LIGHT_SELECTION, LIGHT_FOREGROUND, LIGHT_BACKGROUND)
+			val DEFAULTS_DARK = Colors(DARK_RED, DARK_RED, DARK_GREEN, DARK_GREEN_SHADOW, DARK_RED, DARK_GREEN, DARK_RED, DARK_RED_SHADOW, DARK_SELECTION, DARK_FOREGROUND, DARK_BACKGROUND)
 		}
 	}
 
