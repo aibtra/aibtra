4850f304c95b0f6dd9a012c45d156ead21d3af37
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-11-23 15:54:25 +0100

    Refactoring HotkeyListener: move to dev.aibtra.main.frame

--- src/main/kotlin/dev/aibtra/main/startup/MainApplication.kt
+++ src/main/kotlin/dev/aibtra/main/startup/MainApplication.kt
@@ -6,7 +6,6 @@ package dev.aibtra.main.startup
 
 import dev.aibtra.configuration.*
 import dev.aibtra.core.*
-import dev.aibtra.gui.*
 import dev.aibtra.main.frame.*
 import kotlinx.coroutines.*
 import java.util.*
