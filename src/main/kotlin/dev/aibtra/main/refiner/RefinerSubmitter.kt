7c2ee31f5271bc53d2f65a7789e1a3180055601f
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-12-05 09:24:23 +0100

    OpenAIService: introduce, containing refinement-independent code

--- src/main/kotlin/dev/aibtra/main/refiner/RefinerSubmitter.kt
+++ src/main/kotlin/dev/aibtra/main/refiner/RefinerSubmitter.kt
@@ -4,15 +4,9 @@
 
 package dev.aibtra.main.refiner
 
-import dev.aibtra.configuration.*
-import dev.aibtra.gui.*
 import dev.aibtra.gui.dialogs.*
-import dev.aibtra.gui.dialogs.Panel
 import dev.aibtra.main.content.*
 import dev.aibtra.openai.*
-import java.awt.*
-import javax.swing.*
-import javax.swing.event.*
 
 class RefinerSubmitter(private val environment: Environment, private val requestManager: RefinerRequestManager, private val commandControl: RefinerCommandControl, private val dialogDisplayer: DialogDisplayer, val profile: () -> OpenAIRefinementConfiguration.Profile) {
 	init {
@@ -23,64 +17,11 @@ class RefinerSubmitter(private val environment: Environment, private val request
 	}
 
 	fun run() {
-		val configurationProvider = environment.configurationProvider
-		val credentials = configurationProvider.get(OpenAICredentials)
-		if (credentials.apiToken != null) {
-			submit(configurationProvider, dialogDisplayer)
-		}
-		else {
-			OkCancelDialog("API Token") {
-				val panel = Panel(3, 3)
-				val width = if (GuiConfiguration.Fonts.DEFAULT_FONT_SIZE < 16) 400 else 800
-
-				val editorPane = JEditorPane("text/html", "<html><body style='width: $width'><b>Please provide an API token to access the OpenAI API.</b><br><br>It's recommended to create a dedicated token for this purpose to track usage. The token will be <b>stored in plaintext</b> in a configuration file on your local disk!<br><br>To create a token, follow <a href=\"https://platform.openai.com/settings/organization/api-keys\">this link</a>.</body></html>.")
-				editorPane.isEditable = false
-				editorPane.margin = null
-				editorPane.border = null
-				editorPane.isFocusable = false
-				editorPane.addHyperlinkListener { e ->
-					if (e.eventType == HyperlinkEvent.EventType.ACTIVATED) {
-						if (Desktop.isDesktopSupported()) {
-							Desktop.getDesktop().browse(e.url.toURI())
-						}
-					}
-				}
-
-				var row = 0
-				panel.add(editorPane, row++, 0, span = 3)
-				panel.addSeparatorRow(row++)
-
-				val editor = Editor.password("Token")
-				panel.add(editor, row, 0)
-
-				OkCancelDialog.Content(panel) {
-					val token: String = editor.text
-					configurationProvider.change(OpenAICredentials) {
-						it.copy(apiToken = token)
-					}
-
-					submit(configurationProvider, dialogDisplayer)
-				}
-			}.show(dialogDisplayer)
-		}
-	}
-
-	private fun submit(configurationProvider: ConfigurationProvider, dialogDisplayer: DialogDisplayer) {
-		val apiToken = configurationProvider.get(OpenAICredentials).apiToken
-		require(apiToken != null) { "API token must not be null" }
-
-		val profile = profile()
-		val service = OpenAIRefinementService(apiToken, environment.debugLog)
-		val request = RefinerOpenAIRequest(profile, service, { commandControl.retrieveCommand() }) { failure, mightBeAuthentication ->
-			Ui.runInEdt {
-				if (mightBeAuthentication) {
-					configurationProvider.change(OpenAICredentials) {
-						it.copy(apiToken = null)
-					}
-				}
-				Dialogs.showIOError(failure, dialogDisplayer)
-			}
-		}
-		requestManager.schedule(request)
+		Submitter(environment, dialogDisplayer) { apiToken, failureHandler ->
+			val profile = profile()
+			val service = OpenAIRefinementService(apiToken, environment.debugLog)
+			val request = RefinerOpenAIRequest(profile, service, { commandControl.retrieveCommand() }, failureHandler)
+			requestManager.schedule(request)
+		}.submit()
 	}
 }
---
+++ src/main/kotlin/dev/aibtra/openai/OpenAIService.kt
@@ -0,0 +1,173 @@
+package dev.aibtra.openai
+
+import dev.aibtra.core.*
+import dev.aibtra.core.JsonUtils.Companion.objNotNull
+import org.json.simple.*
+import org.json.simple.parser.*
+import java.io.*
+import java.net.*
+import java.nio.charset.*
+
+open class OpenAIService(private val apiToken: String, private val debugLog: DebugLog) {
+
+	protected fun request(model: String, messages: JSONArray, handler: Handler, failureHandler: FailureHandler) {
+		val input = JSONObject()
+		input["model"] = model
+		input["n"] = 1
+		input["messages"] = messages
+
+		val streaming = handler is StreamingHandler
+		if (streaming) {
+			input["stream"] = true
+		}
+
+		val url = URI("https://api.openai.com/v1/chat/completions").toURL()
+		val connection = url.openConnection() as HttpURLConnection
+		val startTime = System.currentTimeMillis()
+		try {
+			connection.doOutput = true
+			connection.requestMethod = "POST"
+			connection.addRequestProperty("Authorization", "Bearer $apiToken")
+			connection.addRequestProperty("Content-Type", "application/json")
+
+			debugLog.run("openai", "network", DebugLog.Level.INFO) { log: DebugLog.Log, _: Boolean ->
+				val jsonInput = input.toJSONString()
+				log.println("SEND: ")
+				log.println(JsonUtils.formatJson(jsonInput))
+
+				log.println("")
+				log.println("RECEIVE: ")
+				try {
+					connection.outputStream.use { output ->
+						output.write(jsonInput.toByteArray(StandardCharsets.UTF_8))
+						output.flush()
+
+						connection.inputStream.use { input ->
+							when (handler) {
+								is StreamingHandler -> {
+									val reader = BufferedReader(InputStreamReader(input, StandardCharsets.UTF_8))
+									val builder = StringBuilder()
+									while (true) {
+										val line = reader.readLine() ?: break
+										log.println(line)
+
+										if (line.startsWith("data: ")) {
+											val data = line.substring(6)
+											if (data == "[DONE]") {
+												break
+											}
+
+											if (!parseDataChunk(data, builder, handler)) {
+												break
+											}
+										}
+									}
+
+									measureRequestTime(startTime)
+									handler.finish(builder)
+								}
+								is ResultHandler -> {
+									InputStreamReader(input, StandardCharsets.UTF_8).use {
+										val parser = JSONParser()
+										val result = parser.parse(it)
+										log.println(result.toString())
+
+										val choices = objNotNull<JSONArray>(result, "choices")
+										if (choices.size != 1) {
+											throw IOException("Unexpected number of 'choices'")
+										}
+										val choice = requireNotNull(choices[0])
+										val messageOut = objNotNull<JSONObject>(choice, "message")
+										val message = objNotNull<String>(messageOut, "content")
+
+										measureRequestTime(startTime)
+										handler.process(message)
+									}
+								}
+							}
+						}
+					}
+				} catch (ioe: IOException) {
+					val parser = JSONParser()
+					val mightBeAuthentication = connection.responseCode in AUTHENTICATION_RELATED_RESPONSE_CODES
+					connection.errorStream?.let { errorStream ->
+						(parser.parse(InputStreamReader(errorStream, StandardCharsets.UTF_8)) as? JSONObject)?.let { result ->
+							(result["error"] as? JSONObject)?.let { error ->
+								(error["message"] as? String)?.let { message ->
+									failureHandler.process(IOException("${ioe.message}:\n\n$message"), mightBeAuthentication)
+								}
+							}
+						}
+					} ?: run {
+						if (ioe is UnknownHostException) {
+							failureHandler.process(IOException("Unknown host: ${ioe.message}"), false)
+						}
+						else {
+							failureHandler.process(ioe, mightBeAuthentication)
+						}
+					}
+				}
+			}
+		} finally {
+			connection.disconnect()
+		}
+	}
+
+	private fun measureRequestTime(startTime: Long) {
+		println("REQUEST finished in ${System.currentTimeMillis() - startTime}ms ==============")
+	}
+
+	private fun parseDataChunk(data: String, builder: StringBuilder, handler: StreamingHandler): Boolean {
+		return StringReader(data).use {
+			val parser = JSONParser()
+			val result = parser.parse(it)
+			val choices = objNotNull<JSONArray>(result, "choices")
+			if (choices.size != 1) {
+				throw IOException("Unexpected number of 'choices'")
+			}
+
+			val choice = requireNotNull(choices[0])
+			val messageOut = objNotNull<JSONObject>(choice, "delta")
+			if (JsonUtils.objMaybeNull<String>(choice, "finish_reason") == null) {
+				val message = objNotNull<Any>(messageOut, "content")
+				builder.append(message)
+				handler.process(builder)
+			}
+			else {
+				false
+			}
+		}
+	}
+
+	sealed interface Handler {
+	}
+
+	interface StreamingHandler : Handler {
+		fun process(builder: StringBuilder): Boolean
+
+		fun finish(builder: StringBuilder)
+	}
+
+	interface ResultHandler : Handler {
+		fun process(message: String)
+	}
+
+	interface FailureHandler {
+		fun process(failure: IOException, mightBeAuthentication: Boolean)
+	}
+
+	companion object {
+		private val AUTHENTICATION_RELATED_RESPONSE_CODES = setOf(HttpURLConnection.HTTP_UNAUTHORIZED, HttpURLConnection.HTTP_FORBIDDEN)
+
+		fun addMessage(content: String, role: OpenAIRole, array: JSONArray) {
+			array.add(createMessage(content, role))
+		}
+
+		fun createMessage(content: String, role: OpenAIRole): JSONObject {
+			val message = JSONObject()
+			message["role"] = role.id
+			message["content"] = content
+			return message
+		}
+	}
+}
