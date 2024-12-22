ffd81413eff580e438d3915c718c6b97da089d47
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-12-22 12:46:14 +0100

    Refiner: try to preserve caret position after toggling the Before/After display, at least to some extent

--- src/main/kotlin/dev/aibtra/main/refiner/RefinerTextualTab.kt
+++ src/main/kotlin/dev/aibtra/main/refiner/RefinerTextualTab.kt
@@ -71,7 +71,7 @@ internal abstract class RefinerTextualTab(initialWorkingMode: WorkingMode, priva
 				}
 			}
 
-			refTextArea.setText(state.refFormatted, state.refChars)
+			refTextArea.setText(state.refFormatted, state.refChars, lastState.diff.ref == state.diff.ref )
 
 			Ui.runInEdt {
 				if (state.diff.refFinished && !lastState.diff.refFinished) {
