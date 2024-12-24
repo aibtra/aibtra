b458c8ff687723089ed925e781676285d2f6152f
Marc Strapetz <3380730+mstrap@users.noreply.github.com>
2024-12-24 11:01:21 +0100

    RefinerTab: slightly change logic

--- src/main/kotlin/dev/aibtra/main/refiner/RefinerFileTab.kt
+++ src/main/kotlin/dev/aibtra/main/refiner/RefinerFileTab.kt
@@ -21,8 +21,8 @@ internal class RefinerFileTab(tabbedPane: MainTabbedPane, environment: Environme
 		workFile.addStateListener { state ->
 			if (state != null && state.initial) {
 				val text = state.content
-				rawEditor.setText(text)
 				diffManager.updateRawText(text, null, profileManager.profile().diffConfig, RefinerDiffManager.Normalization.STOP, null)
+				rawEditor.setText(text)
 
 				state.initialLine?.let {
 					// Needs to be postponed to function correctly
